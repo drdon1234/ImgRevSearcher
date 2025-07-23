b7c57aab8953bcba115f93f5d36b7130191af45d
don <doncaca5@outlook.com>
2025-07-23 18:35:49 +0800

    添加了自动绘制搜索结果的函数

---
+++ test_templates/test_search_draw_func.py
@@ -0,0 +1,10 @@
+import asyncio
+from model import BaseSearchModel
+
+
+async def test_search_and_draw():
+    model = BaseSearchModel()
+    await model.search_and_draw(api="google_lens", file="test_img.jpg", is_auto_save=True)
+
+if __name__ == "__main__":
+    asyncio.run(test_search_and_draw())
